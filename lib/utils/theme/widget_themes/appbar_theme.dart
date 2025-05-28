867ea1bf2cc1929c75cee8ffa421168bf26df02d
manoj-kumar-m <tech@tosyncup.com>
2025-05-28 10:35:26 +0530

    [feat]: splash screen added

--- lib/utils/theme/widget_themes/appbar_theme.dart
+++ lib/utils/theme/widget_themes/appbar_theme.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:t_store/utils/constants/sizes.dart';
+import 'package:ecommerce/utils/constants/sizes.dart';
 import '../../constants/colors.dart';
 
 class TAppBarTheme{
